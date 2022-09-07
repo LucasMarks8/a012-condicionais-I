99ddad455f2bc6dd37fb910c566629ba528cecf9
LucasMarks8 <lucas.marks8@gmail.com>
2022-09-07 09:36:41 -0300

    Exercício a012 finalizado

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,5 +1,15 @@
-const num = //seu numero aqui
+// const num = //seu numero aqui
 
-if(){
-    console.log("sua mensagem aqui");
+// if(){
+//     console.log("sua mensagem aqui");
+// }
+// # Exercício 1
+
+// Crie um código que receba uma const numérica qualquer. Crie um `if` para verificar se o número guardado na const é **par**. 
+// Caso seja, imprima no console a mensagem: "o número é par".
+
+const numero = 10
+
+if (numero % 2 === 0){
+    console.log("O número é par")
 }
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,38 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+// Escreva um código que receba três valores. Uma idade, um booleano que responda se a pessoa terminou ou não o ensino médio, e um booleano que responda se a pessoa está cursando alguma faculdade.
 
-if(){
+// Crie um if para cada variável, checando as seguintes afirmacões:
 
+// Se a pessoa tem 18 anos ou mais;
+// Se a pessoa terminou o ensino médio;
+// Se a pessoa NÃO está cursando alguma faculdade;
+// Crie um if que imprima que a afirmacão é verdadeira, e um else para imprimir que a afirmacão não é verdadeira.
+
+// Exemplo
+// Caso a primeira pessoa tenha idade >=18:
+
+// "A pessoa é maior de idade"
+
+// e caso não seja:
+
+// "A pessoa é menor de idade"
+let idade = prompt("Qual é a sua idade?")
+let isMaiorIdade = prompt("Você já concluiu o ensino médio? Se sim, digite S e se não, digite N")
+let isCursandoOutraFaculdade = prompt("Voc^está cursando faculdade? Se sim, digite S e se não, digite N")
+
+if(idade >= 18){
+    console.log("Você é maior de idade")
 }else{
+    console.log("Você é menor de idade")
+}
 	
+if(isMaiorIdade === "S"){
+    console.log("Você concluiu o ensino médio, parabéns!")
+}else{
+    console.log("Você não concluiu o ensino médio")
+}
+
+if(isCursandoOutraFaculdade === "S"){
+    console.log("Você está cursando faculdade")
+}else{
+    console.log("Você não está cursando faculdade")
 }
